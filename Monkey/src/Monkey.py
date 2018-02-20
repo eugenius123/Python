f32e5b0dd550b6f103f59ace7a3d9c322812f8a6
eugenius123 <yugene123@gmail.com>
2018-02-20 13:45:19 -0500

    Finished Monkey

--- Monkey/src/Monkey.py
+++ Monkey/src/Monkey.py
@@ -13,6 +13,7 @@ def Gen(n):
         sentence = sentence + pool[random.randrange(len(pool))]
     return sentence
 
+#the score is not cool
 def Score(phrase,actual):
     score = 0
     for k in range(len(actual)):
@@ -24,25 +25,29 @@ def Score(phrase,actual):
 def Improve(phrase,actual):
     pool = "abcdefghijklmnopqrstuvwxyz "
     sentence = list(phrase)
+    counter = 0
     for k in range(len(actual)):
         if sentence[k] != actual[k]:
-            sentence[k] = pool[random.randrange(len(pool))]
-            newsentence = ''.join(sentence)
-            return newsentence
-        else:
-            return phrase
+            counter = counter + 1
+    if counter != 0:
+        for k in range(len(actual)):
+            if sentence[k] != actual[k]:
+                randchoice = pool[random.randrange(len(pool))]
+                sentence[k] = randchoice
+                newsentence = ''.join(sentence)
+                return newsentence
+    else:
+        return phrase
+
 def main():
     statement = "methinks it is like a weasel"
-    current_score=0
     
     newstring = Gen(28)
     newscore = Score(newstring,statement)
     while newscore < 1:
-        if newscore > current_score:
-            print(newstring + " score is " + str(newscore))
-            current_score = newscore
-        newstring = Improve(newstring,statement)
+        currentstring=newstring
+        print(currentstring + " score is " + str(newscore))
+        newstring = Improve(currentstring,statement)
         newscore = Score(newstring,statement)
+    print(newstring)
 main()
-# pool = "abcdefghijklmnopqrstuvwxyz "
-# print (pool)
